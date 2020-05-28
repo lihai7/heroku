a88aa20c88653fcabea7e741f2afa405a1350ed7
lihai7 <41443979+lihai7@users.noreply.github.com>
2020-05-28 18:33:45 +0300

    Update weatherBOT.py

--- weatherBOT.py
+++ weatherBOT.py
@@ -5,7 +5,7 @@ import requests
 import re
 
 def parse_btc_to_usd():
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                       '(KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
     }
     url = 'https://bit.ly/36w63nL'
